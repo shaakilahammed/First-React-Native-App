3cfc76ab6a6842727b3a34b7e850906eb19cca3c
shaakilahammed <shaakilahammed@gmail.com>
2021-11-06 00:27:26 +0600

    First Commit

--- App.js
+++ App.js
@@ -1,21 +1,129 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Platform,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  StatusBar,
+  Dimensions,
+} from 'react-native';
+import {
+  useDimensions,
+  useDeviceOrientation,
+} from '@react-native-community/hooks';
+import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  return <ViewImageScreen />;
+  // return <WelcomeScreen />;
+  // // const handlePress = () => console.log(require('./assets/favicon.png'));
+  // // console.log(Dimensions.get('window'));
+  // // console.log(Dimensions.get('screen'));
+  // const { landscape } = useDeviceOrientation();
+  // return (
+  //   // <SafeAreaView style={styles.container}>
+  //   //   {/* <TouchableNativeFeedback>
+  //   //     <View
+  //   //       style={{
+  //   //         paddingBottom: 20,
+  //   //         paddingTop: 20,
+  //   //         paddingLeft: 50,
+  //   //         paddingRight: 50,
+  //   //         backgroundColor: '#fff',
+  //   //       }}
+  //   //     >
+  //   //       <Text numberOfLines={1} onPress={handlePress}>
+  //   //         Hello React!
+  //   //       </Text>
+  //   //     </View>
+  //   //   </TouchableNativeFeedback> */}
+  //   //   {/* <Image source={require('./assets/favicon.png')} /> */}
+  //   //   {/* <TouchableNativeFeedback onPress={() => console.log('ITapped')}>
+  //   //     <Image
+  //   //       source={{
+  //   //         width: 200,
+  //   //         height: 200,
+  //   //         uri: 'https://picsum.photos/200',
+  //   //       }}
+  //   //     />
+  //   //   </TouchableNativeFeedback> */}
+  //   //   {/* <Button
+  //   //     color="red"
+  //   //     title="Click"
+  //   //     // onPress={() =>
+  //   //     //   Alert.alert('Title', 'Message', [
+  //   //     //     { text: 'Yes', onPress: () => console.log('Yes') },
+  //   //     //     { text: 'No', onPress: () => console.log('No') },
+  //   //     //   ])
+  //   //     // }
+  //   //     onPress={() =>
+  //   //       Alert.prompt('Tytle', 'What is your Name?', (text) =>
+  //   //         console.log(text)
+  //   //       )
+  //   //     }
+  //   //   /> */}
+  //   //   <Button title="click" onPress={() => console.log('PRessed')} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  //   //   <View
+  //   //     style={{
+  //   //       backgroundColor: '#000',
+  //   //       width: '50%',
+  //   //       height: landscape ? '10%' : '20%',
+  //   //     }}
+  //   //   ></View>
+  //   // </SafeAreaView>
+  //   <View
+  //     style={{
+  //       backgroundColor: 'blue',
+  //       flex: 1,
+  //       flexDirection: 'row',
+  //       justifyContent: 'center',
+  //       alignItems: 'center',
+  //       alignContent: 'center',
+  //       // flexWrap: 'wrap',
+  //     }}
+  //   >
+  //     <View
+  //       style={{ backgroundColor: 'tomato', height: 100, width: 100 }}
+  //     ></View>
+  //     <View
+  //       style={{
+  //         backgroundColor: 'black',
+  //         flexBasis: 300,
+  //         // flexGrow: 1,
+  //         flexShrink: -1,
+  //         // flex: 1,
+  //         height: 100,
+  //         // width: 100,
+  //         top: 20,
+  //         left: 20,
+  //         position: 'relative',
+  //       }}
+  //     ></View>
+  //     <View
+  //       style={{
+  //         backgroundColor: 'red',
+  //         height: 100,
+  //         width: 100,
+  //         // alignSelf: 'flex-start',
+  //       }}
+  //     ></View>
+  //   </View>
+  // );
+}
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#f7b731',
+//     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+//   },
+// });
