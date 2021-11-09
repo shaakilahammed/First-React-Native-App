0e8df6352ce19f60e5e44f591de1cbe28bb915ef
shaakilahammed <shaakilahammed@gmail.com>
2021-11-09 17:01:56 +0600

    Styling done

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -2,20 +2,20 @@ import React from 'react';
 import { Platform, Text, StyleSheet } from 'react-native';
 import colors from '../config/colors';
 
-function AppText({ children }) {
-  return <Text style={styles.text}>{children}</Text>;
+function AppText({ children, style }) {
+  return <Text style={[styles.text, style]}>{children}</Text>;
 }
 
 const styles = StyleSheet.create({
   text: {
-    color: colors.secondary,
+    color: colors.black,
     ...Platform.select({
       ios: {
-        fontSize: 30,
+        fontSize: 20,
         fontFamily: 'Avenir',
       },
       android: {
-        fontSize: 20,
+        fontSize: 18,
         fontFamily: 'Roboto',
       },
     }),
