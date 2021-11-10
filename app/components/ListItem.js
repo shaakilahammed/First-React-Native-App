0ba835bd5ffe72383a710e8cb8cbb523a5e3195a
shaakilahammed <shaakilahammed@gmail.com>
2021-11-10 22:22:09 +0600

    Profile Screen

--- app/components/ListItem.js
+++ app/components/ListItem.js
@@ -33,6 +33,7 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
     padding: 15,
+    backgroundColor: colors.white,
   },
   detailsContainer: {
     marginLeft: 10,
--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -1,17 +1,71 @@
 import React from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
+import Icon from '../components/Icon';
+import ListItemSeparator from '../components/ListItemSeparator';
+import colors from '../config/colors';
 
+const menuItems = [
+  {
+    title: 'My Listings',
+    icon: {
+      name: 'format-list-bulleted',
+      backgroundColor: colors.primary,
+    },
+  },
+  {
+    title: 'My Messages',
+    icon: {
+      name: 'email',
+      backgroundColor: colors.secondary,
+    },
+  },
+];
 function AccountScreen(props) {
   return (
-    <Screen>
+    <Screen style={styles.screen}>
+      <View style={styles.container}>
+        <ListItem
+          title="Mosh Hamedani"
+          subTitle="programmingwithmosh@gmail.com"
+          image={require('../assets/mosh.jpg')}
+        />
+      </View>
+      <View style={styles.container}>
+        <FlatList
+          data={menuItems}
+          keyExtractor={(menuItem) => menuItem.title}
+          renderItem={({ item }) => (
+            <ListItem
+              title={item.title}
+              IconComponent={
+                <Icon
+                  name={item.icon.name}
+                  backgroundColor={item.icon.backgroundColor}
+                />
+              }
+            />
+          )}
+          ItemSeparatorComponent={ListItemSeparator}
+        />
+      </View>
+
       <ListItem
-        title="Mosh Hamedani"
-        subTitle="programmingwithmosh@gmail.com"
-        image={require('../assets/mosh.jpg')}
+        title="Logout"
+        IconComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
       />
     </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 20,
+  },
+  screen: {
+    backgroundColor: colors.light,
+  },
+});
 
 export default AccountScreen;
