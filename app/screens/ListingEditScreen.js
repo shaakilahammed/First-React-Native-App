2947aebd1eec7e68a95ab35c1785f101189c34f9
shaakilahammed <shaakilahammed@gmail.com>
2021-12-03 18:43:20 +0600

    Show done animation after posting

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -94,13 +94,19 @@ function ListingEditScreen(props) {
       { ...listing, location },
       (progress) => setProgress(progress)
     );
-    setUploadVisible(false);
-    if (!result.ok) return alert('Could not save the listing');
-    alert('Success');
+
+    if (!result.ok) {
+      setUploadVisible(false);
+      return alert('Could not save the listing');
+    }
   };
   return (
     <Screen style={styles.container}>
-      <UploadScreen progress={progress} visible={uploadVisible} />
+      <UploadScreen
+        onDone={() => setUploadVisible(false)}
+        progress={progress}
+        visible={uploadVisible}
+      />
       <AppForm
         initialValues={{
           title: '',
