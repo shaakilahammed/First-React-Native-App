bc7a6f91fc22da1c064082d1ae50479779dcebb0
shaakilahammed <shaakilahammed@gmail.com>
2021-12-03 19:01:57 +0600

    Reset Form

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -87,7 +87,7 @@ function ListingEditScreen(props) {
   const [uploadVisible, setUploadVisible] = useState(false);
   const [progress, setProgress] = useState(0);
 
-  const handleSubmit = async (listing) => {
+  const handleSubmit = async (listing, { resetForm }) => {
     setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
@@ -99,6 +99,8 @@ function ListingEditScreen(props) {
       setUploadVisible(false);
       return alert('Could not save the listing');
     }
+
+    resetForm();
   };
   return (
     <Screen style={styles.container}>
